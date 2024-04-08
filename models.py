0c4e052c52c2867781b2a18b2f2aad5ac43db65d
dengxiangyu <dengxiangyu@zonst.cn>
2024-04-08 16:58:46 +0800

    update

--- models.py
+++ models.py
@@ -21,9 +21,11 @@ class Todo(db.Model):
 
 class Progress(db.Model):
     id = db.Column(db.Integer,primary_key=True) 
-    name = db.Column(db.String(100),nullable=False)
+    name = db.Enum('冲压', '焊接', '涂装', '总装', '完成')
+   
     create_timestamp = db.Column(db.String(20), nullable=False)
     modify_timestamp = db.Column(db.String(20))
+    product_id = db.Column(db.Integer,db.ForeignKey("product.id"))
 
     def to_dict(self):
         return {"id":self.id,"name": self.name,"create_timestamp": self.create_timestamp,"modify_timestamp": self.modify_timestamp}
@@ -36,7 +38,25 @@ class Progress(db.Model):
         db.session.delete(self)
         db.session.commit()
 
+class Product(db.Model):
+    id = db.Column(db.Integer,primary_key=True) 
+    name = db.Column(db.String(100),nullable=False)
 
+    create_timestamp = db.Column(db.String(20), nullable=False)
+    completion_timestamp = db.Column(db.String(20))
+    progress_id = db.relationship("Progress",backref="product",lazy="dynamic")
+
+
+    def to_dict(self):
+        return {"id":self.id,"name": self.name,"create_timestamp": self.create_timestamp,"modify_timestamp": self.modify_timestamp}
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
 # class User(db.Model):
     
